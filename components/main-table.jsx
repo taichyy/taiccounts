4321c40ac666599fb5dfb3f4b73f71e07f3067c4
taichyy <chinzeilulu@gmail.com>
2023-10-24 18:38:13 +0800

    Set data revalidate time to 0, make loading screen clear.

--- components/main-table.jsx
+++ components/main-table.jsx
@@ -25,7 +25,9 @@ const MainTable = () => {
     const fetcher = (...args) => fetch(...args).then(res => res.json()).then( res => filter(res))
     const { data, mutate, error, isLoading } = useSWR(
         `/api/accounts`, 
-        fetcher
+        fetcher,{
+            refreshInterval : 2000,
+        }
     )
 
     useEffect(()=>{
@@ -59,52 +61,61 @@ const MainTable = () => {
         if( e.target.value == '' ) {
             setFiltered(data)
         } else {
-            setFiltered(data.filter( record => record.title.includes(e.target.value)))
+            setFiltered(
+                data.filter( 
+                    record => 
+                        record.title.toLowerCase().includes(e.target.value.toLowerCase()) || 
+                        record.username.toLowerCase().includes(e.target.value.toLowerCase())
+                    ))
         }
     }
 
 
     return (
     <div>
-        <Input onChange={(e)=>handleChange(e)}/>
-        <Table>
-            <TableHeader className="border-t-2 border-b-2">
-                <TableRow>
-                    <TableHead className="w-fit"></TableHead>
-                    <TableHead className="w-fit">帳號</TableHead>
-                    <TableHead>密碼</TableHead>
-                    <TableHead></TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                { sessionStorage.getItem("isLogined") ? filtered?.sort((a,b) => a.title.localeCompare(b.title)).map( acc => (
-                    <TableRow key={acc._id}>
-                        <TableCell className="w-fit">
-                            {acc.title.split('、').map( (line, index) => (
-                                <span key={index}>
-                                    {line}
-                                    { index == acc.title.split('、').length-1 ? '' : '、'}
-                                    <br/>
-                                </span>
-                            ))}
-                        </TableCell>
-                        <TableCell className="w-fit">{acc.username}</TableCell>
-                        <TableCell>
-                            { verified ? 
-                                acc.password : 
-                                <TwoFA id={acc._id} verified={verified} setVerified={setVerified}/>
-                            }
-                        </TableCell>
-                        <TableCell>
-                            { editVerified ? 
-                                <EditForm id={acc._id} mainMutate={mutate}/> : 
-                                <Edit id={acc._id} verified={editVerified} setVerified={setEditVerified}/>
-                            }
-                        </TableCell>
+        {sessionStorage.getItem("isLogined") ? (
+            <>
+            <Input onChange={(e)=>handleChange(e)}/>
+            <Table>
+                <TableHeader className="border-t-2 border-b-2">
+                    <TableRow>
+                        <TableHead className="w-fit"></TableHead>
+                        <TableHead className="w-fit">帳號</TableHead>
+                        <TableHead>密碼</TableHead>
+                        <TableHead></TableHead>
                     </TableRow>
-                )) : null}
-            </TableBody>
-        </Table>
+                </TableHeader>
+                <TableBody>
+                    { filtered?.sort((a,b) => a.title.localeCompare(b.title)).map( acc => (
+                        <TableRow key={acc._id}>
+                            <TableCell className="w-fit">
+                                {acc.title.split('、').map( (line, index) => (
+                                    <span key={index}>
+                                        {line}
+                                        { index == acc.title.split('、').length-1 ? '' : '、'}
+                                        <br/>
+                                    </span>
+                                ))}
+                            </TableCell>
+                            <TableCell className="w-fit">{acc.username}</TableCell>
+                            <TableCell>
+                                { verified ? 
+                                    acc.password : 
+                                    <TwoFA id={acc._id} verified={verified} setVerified={setVerified}/>
+                                }
+                            </TableCell>
+                            <TableCell>
+                                { editVerified ? 
+                                    <EditForm id={acc._id} mainMutate={mutate}/> : 
+                                    <Edit id={acc._id} verified={editVerified} setVerified={setEditVerified}/>
+                                }
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            </>
+        ) : null}
     </div>
     );
 }
