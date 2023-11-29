b9eb9b4939dadf36f5b1b412a59ce6ab54b41ba1
taichyy <chinzeilulu@gmail.com>
2023-11-29 22:09:14 +0800

    Try to disable shrinking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
 
 import './globals.css'
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </Head>
       <body>
         {children}
       </body>
